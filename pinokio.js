ebeab202db8548d1c5a7623f9e54e3748e3b068c
De2Housing <parjanya.shukla@housing.com>
2023-12-22 14:13:19 +0530

    Added Image, Changed UI on Pinokio

---
+++ pinokio.js
@@ -0,0 +1,5 @@
+module.exports = {
+    title: "Housing App Android Setup",
+    description: "Unlock the new experience of Housing App Android Setup with Automation using Pinokio",
+    icon: "housing.jpg",
+}
